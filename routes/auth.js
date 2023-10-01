d4700f2832e4d14d78045a53a74175a1bd0b9f67
KRG17 <kritankrishigoel@gmail.com>
2023-10-01 16:51:48 +0530

    commit

--- routes/auth.js
+++ routes/auth.js
@@ -1,47 +1,47 @@
-const express = require("express");
-const router = express.Router();
-const User = require("../models/User");
-const bcrypt = require("bcrypt");
-const jwt = require("jsonwebtoken");
+const express = require("express")
+const router = express.Router()
+const User = require("../models/User")
+const bcrypt = require("bcrypt")
+const jwt = require("jsonwebtoken")
 
 //REGISTER
 router.post("/register", async (req, res) => {
   try {
-    const { username, email, password } = req.body;
-    const salt = await bcrypt.genSalt(10);
-    const hashedPassword = await bcrypt.hashSync(password, salt);
-    const newUser = new User({ username, email, password: hashedPassword });
-    const savedUser = await newUser.save();
-    res.status(200).json(savedUser);
+    const { username, email, password } = req.body
+    const salt = await bcrypt.genSalt(10)
+    const hashedPassword = await bcrypt.hashSync(password, salt)
+    const newUser = new User({ username, email, password: hashedPassword })
+    const savedUser = await newUser.save()
+    res.status(200).json(savedUser)
   } catch (err) {
-    res.status(500).json(err);
+    res.status(500).json(err)
   }
-});
+})
 
 //LOGIN
 router.post("/login", async (req, res) => {
   try {
-    const user = await User.findOne({ email: req.body.email });
+    const user = await User.findOne({ email: req.body.email })
 
     if (!user) {
-      return res.status(404).json("User not found!");
+      return res.status(404).json("User not found!")
     }
-    const match = await bcrypt.compare(req.body.password, user.password);
+    const match = await bcrypt.compare(req.body.password, user.password)
 
     if (!match) {
-      return res.status(401).json("Wrong credentials!");
+      return res.status(401).json("Wrong credentials!")
     }
     const token = jwt.sign(
       { _id: user._id, username: user.username, email: user.email },
       process.env.SECRET,
       { expiresIn: "3d" }
-    );
-    const { password, ...info } = user._doc;
-    res.cookie("token", token).status(200).json(info);
+    )
+    const { password, ...info } = user._doc
+    res.cookie("token", token).status(200).json(info)
   } catch (err) {
-    res.status(500).json(err);
+    res.status(500).json(err)
   }
-});
+})
 
 //LOGOUT
 router.get("/logout", async (req, res) => {
@@ -49,22 +49,22 @@ router.get("/logout", async (req, res) => {
     res
       .clearCookie("token", { sameSite: "none", secure: true })
       .status(200)
-      .send("User logged out successfully!");
+      .send("User logged out successfully!")
   } catch (err) {
-    res.status(500).json(err);
+    res.status(500).json(err)
   }
-});
+})
 
 //REFETCH USER
 router.get("/refetch", (req, res) => {
-  const token = req?.cookies?.token;
+  const token = req.cookies.token
   //   console.log(token);
   jwt.verify(token, process.env.SECRET, {}, async (err, data) => {
     if (err) {
-      return res.status(404).json(err);
+      return res.status(404).json(err)
     }
-    res.status(200).json(data);
-  });
-});
+    res.status(200).json(data)
+  })
+})
 
-module.exports = router;
+module.exports = router
